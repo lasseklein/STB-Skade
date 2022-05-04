77b15dcf2bd75466807cf2b4d2a15a8cb4da86a6
LK <nonspammabile@gmail.com>
2022-05-04 19:42:47 +0200

    test

--- skade.js
+++ skade.js
@@ -1,4 +1,12 @@
-var Mustache = require('mustache');
+//Uses Mustache
+var view = {
+  title: "Joe",
+  calc: function () {
+    return 2 + 4;
+  }
+};
+
+var output = Mustache.render("{{title}} spends {{calc}}", view);
 
 var scoreneedle = $("#needle");
 
