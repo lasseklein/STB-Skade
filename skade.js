39cde87d1eb60536459312cb7e73c00ab233f07a
LK <nonspammabile@gmail.com>
2022-05-05 13:31:12 +0200

    t

--- skade.js
+++ skade.js
@@ -4,22 +4,27 @@
 var opts = {
   angle: -0.2, // The span of the gauge arc
   animationSpeed: 11,
-  lineWidth: 0.44, // The line thickness
+  lineWidth: 0.23, // The line thickness
   radiusScale: 1, // Relative radius
   pointer: {
     length: 0.6, // // Relative to gauge radius
     strokeWidth: 0.035, // The thickness
     color: '#000000' // Fill color
   },
-  limitMax: false,     // If false, max value increases automatically if value > maxValue
-  limitMin: false,     // If true, the min value of the gauge will be fixed
+  limitMax: 0,     // If false, max value increases automatically if value > maxValue
+  limitMin: 100,     // If true, the min value of the gauge will be fixed
   colorStart: '#6FADCF',   // Colors
   colorStop: '#8FC0DA',    // just experiment with them
   strokeColor: '#E0E0E0',  // to see which ones work best for you
   generateGradient: true,
   highDpiSupport: true,     // High resolution support
   percentColors : [[0.0, "#a9d70b" ], [0.50, "#f9c802"], [1.0, "#ff0000"]],
-  
+  staticZones: [
+    {strokeStyle: "#F03E3E", min: 0, max: 30},
+    {strokeStyle: "#FFDD00", min: 30, max: 65},
+    {strokeStyle: "#30B32D", min: 65, max: 100}
+ ],
+
 };
 var target = document.getElementById('gaugecanvas'); // your canvas element
 var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
